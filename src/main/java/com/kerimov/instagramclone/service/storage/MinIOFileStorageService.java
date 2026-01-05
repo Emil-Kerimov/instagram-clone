49b1540c69df28a52dd12d004599ea323af83cc3
Emil Kerimov <emilkerimovofficial@gmail.com>
2026-01-05 22:43:35 +0200

    add createBucketIfNotExists ,ethod in FileStorageService

--- src/main/java/com/kerimov/instagramclone/service/storage/MinIOFileStorageService.java
+++ src/main/java/com/kerimov/instagramclone/service/storage/MinIOFileStorageService.java
@@ -1,12 +1,18 @@
 package com.kerimov.instagramclone.service.storage;
 
 import com.kerimov.instagramclone.exceptions.FileStorageServiceException;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.awscore.exception.AwsServiceException;
+import software.amazon.awssdk.core.exception.SdkClientException;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
+import software.amazon.awssdk.services.s3.model.HeadBucketRequest;
+import software.amazon.awssdk.services.s3.model.NoSuchBucketException;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.IOException;
@@ -20,7 +26,7 @@ public class MinIOFileStorageService implements IMinIOFileStorageService {
     @Value("${minio.bucket.name}")
     private String bucketName;
 
-    @Value("${minio.url}")
+    @Value("${minio.external.url}")
     private String url;
 
     @Override
@@ -45,6 +51,31 @@ public class MinIOFileStorageService implements IMinIOFileStorageService {
             throw new FileStorageServiceException("loading file to MinIO is not possible");
         }
 
+    }
+
+    @PostConstruct
+    public void init(){
+        createBucketIfNotExists();
+    }
+
+    private void createBucketIfNotExists() {
+        HeadBucketRequest headBucketRequest = HeadBucketRequest.builder()
+                .bucket(bucketName)
+                .build();
+        try{
+            s3Client.headBucket(headBucketRequest);
+        } catch (NoSuchBucketException e) {
+            try {
+                CreateBucketRequest createBucketRequest = CreateBucketRequest.builder()
+                        .bucket(bucketName)
+                        .build();
+
+                s3Client.createBucket(createBucketRequest);
+            } catch (Exception ex) {
+                throw new FileStorageServiceException("Cannot create bucket");
+            }
+        } catch (Exception e) {
+        throw new FileStorageServiceException("Cannot check bucket exists");}
     }
 
     @Override
