f4365d8189a90121d529bbbef6677444b24e9ae1
Emil Kerimov <emilkerimovofficial@gmail.com>
2026-01-11 15:41:17 +0200

    perf: Ñ optimize post fetching using @EntityGraph in PostRepository

--- src/main/java/com/kerimov/instagramclone/repository/PostRepository.java
+++ src/main/java/com/kerimov/instagramclone/repository/PostRepository.java
@@ -1,11 +1,20 @@
 package com.kerimov.instagramclone.repository;
 
 import com.kerimov.instagramclone.models.Post;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PostRepository extends JpaRepository<Post, UUID> {
 
+    @Override
+    @EntityGraph(attributePaths = {"user", "images"}) // TODO: Benchmark
+    List<Post> findAll();
+
+    @Override
+    @EntityGraph(attributePaths = {"user", "images"})
+    Optional<Post> findById(UUID id);
 }
