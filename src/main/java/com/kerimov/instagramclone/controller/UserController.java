c65dec04ce5668c26b837b3bf47fd580f5bfa99e
Emil Kerimov <emilkerimovofficial@gmail.com>
2026-01-15 23:56:53 +0200

    refactor: delete ApiResponse and migrate controllers to clear ResponseEntity

--- src/main/java/com/kerimov/instagramclone/controller/UserController.java
+++ src/main/java/com/kerimov/instagramclone/controller/UserController.java
@@ -3,7 +3,6 @@ package com.kerimov.instagramclone.controller;
 import com.kerimov.instagramclone.dto.UserDto;
 import com.kerimov.instagramclone.request.CreateUserRequest;
 import com.kerimov.instagramclone.request.UpdateUserRequest;
-import com.kerimov.instagramclone.response.ApiResponse;
 import com.kerimov.instagramclone.service.user.IUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 import java.util.UUID;
 
+//TODO: delete Wrapper class
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("${api.prefix}/users")
@@ -22,34 +22,35 @@ public class UserController {
     private final IUserService userService;
 
     @GetMapping
-    public ResponseEntity<ApiResponse<List<UserDto>>> getAllUsers(){
+    public ResponseEntity<List<UserDto>> getAllUsers(){
         List<UserDto> users = userService.getAllUsers();
-        return ResponseEntity.ok(new ApiResponse<>("Successful", users));
+        return ResponseEntity.ok(users);
     }
 
     @GetMapping("/{userId}")
-    public ResponseEntity<ApiResponse<UserDto>> getUserById(@PathVariable UUID userId){
+    public ResponseEntity<UserDto> getUserById(@PathVariable UUID userId){
         UserDto user = userService.getUserById(userId);
-        return ResponseEntity.ok(new ApiResponse<>("Found successful", user));
+        return ResponseEntity.ok(user);
     }
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<ApiResponse<UserDto>> createUser(@RequestPart("request") CreateUserRequest request,
+    public ResponseEntity<UserDto> createUser(@RequestPart("request") CreateUserRequest request,
                            @RequestPart(value = "file", required = false) MultipartFile file){
         UserDto createdUser =  userService.createUser(request, file);
-        return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse<>("user created successful", createdUser));
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
     }
 
     @PatchMapping(path = "/{userId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<ApiResponse<UserDto>> patchUpdateUser(@PathVariable UUID userId, @RequestPart("request") UpdateUserRequest request,
-                              @RequestPart(value = "file", required = false) MultipartFile newAvatar){
+    public ResponseEntity<UserDto> patchUpdateUser(@PathVariable UUID userId,
+                                                   @RequestPart("request") UpdateUserRequest request,
+                                                   @RequestPart(value = "file", required = false) MultipartFile newAvatar){
         UserDto updatedUser = userService.updateUser(userId, request, newAvatar);
-        return ResponseEntity.ok(new ApiResponse<>("user updated successful", updatedUser));
+        return ResponseEntity.ok(updatedUser);
     }
 
     @DeleteMapping("/{userId}")
-    public ResponseEntity<ApiResponse<UserDto>> deleteUserById(@PathVariable UUID userId){
+    public ResponseEntity<Void> deleteUserById(@PathVariable UUID userId){
         userService.deleteUserById(userId);
-        return ResponseEntity.ok(new ApiResponse<>("deleted successful", null));
+        return ResponseEntity.noContent().build();
     }
 }
